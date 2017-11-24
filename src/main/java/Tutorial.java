15fa196032411e5fbb94d45ec1d85b2201835d50
Kamil <magierakm.agh@gmail.com>
2017-11-24 14:00:46 +0100

    juz dziala, tylko bez skip'a

--- src/main/java/Tutorial.java
+++ src/main/java/Tutorial.java
@@ -17,29 +17,22 @@ import uk.co.caprica.vlcj.runtime.RuntimeUtil;
 public class Tutorial {
 
     private final JFrame frame;
+    private static String link;
 
     private final EmbeddedMediaPlayerComponent mediaPlayerComponent;
 
-    private final JButton pauseButton;
 
-    private final JButton rewindButton;
-
-    private final JButton skipButton;
-    private static final String NATIVE_LIBRARY_SEARCH_PATH = "C:\\Program Files\\VideoLAN\\VLC"; //MUSZISZ ZANSTALOWAĆ VLC I DODAĆ TU SCIEZKE DO NIEGO
-    public static void main(final String[] args) {
-        NativeLibrary.addSearchPath(RuntimeUtil.getLibVlcLibraryName(), NATIVE_LIBRARY_SEARCH_PATH);
-        new NativeDiscovery().discover();
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-               // new Tutorial(args);
-                    new Tutorial("https://www.youtube.com/watch?v=huibeWit4gA");
-            }
-        });
+   void pasue(){
+       mediaPlayerComponent.getMediaPlayer().pause();
+   }
+   void play(){
+        mediaPlayerComponent.getMediaPlayer().play();
     }
+    void skip(String time){
+       String tmp = link + "&feature=youtu.be&t=" + time;
+       mediaPlayerComponent.getMediaPlayer().playMedia(tmp); }
 
-   // public Tutorial(String[] args) {
-   public Tutorial(String watchUrl) {
+    public Tutorial(String watchUrl) {
         frame = new JFrame("My First Media Player");
         frame.setBounds(100, 100, 600, 400);
         frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
@@ -50,47 +43,17 @@ public class Tutorial {
                 System.exit(0);
             }
         });
-
+        link = watchUrl;
         JPanel contentPane = new JPanel();
         contentPane.setLayout(new BorderLayout());
 
         mediaPlayerComponent = new EmbeddedMediaPlayerComponent();
         contentPane.add(mediaPlayerComponent, BorderLayout.CENTER);
 
-        JPanel controlsPane = new JPanel();
-        pauseButton = new JButton("Pause");
-        controlsPane.add(pauseButton);
-        rewindButton = new JButton("Rewind");
-        controlsPane.add(rewindButton);
-        skipButton = new JButton("Skip");
-        controlsPane.add(skipButton);
-        contentPane.add(controlsPane, BorderLayout.SOUTH);
-
-        pauseButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                mediaPlayerComponent.getMediaPlayer().pause();
-            }
-        });
-
-        rewindButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                mediaPlayerComponent.getMediaPlayer().skip(-10000);
-            }
-        });
-
-        skipButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                mediaPlayerComponent.getMediaPlayer().skip(10000);
-            }
-        });
 
         frame.setContentPane(contentPane);
         frame.setVisible(true);
 
-       // mediaPlayerComponent.getMediaPlayer().playMedia(args[0]);
         mediaPlayerComponent.getMediaPlayer().setPlaySubItems(true);
         mediaPlayerComponent.getMediaPlayer().playMedia(watchUrl);
     }
